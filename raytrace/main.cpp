0121780bf2158b05cf619202430445fe30d7d499
Michael Reiter <michaelreiter@live.ca>
2018-01-11 16:17:31 -0800

    Set up a1 skeleton

--- raytrace/main.cpp
+++ raytrace/main.cpp
@@ -8,31 +8,47 @@ Colour red() { return Colour(1.0f, 0.0f, 0.0f); }
 Colour white() { return Colour(1.0f, 1.0f, 1.0f); }
 Colour black() { return Colour(0.0f, 0.0f, 0.0f); }
 
-int main(int, char**){
+int main(int, char**) {
+    // number of columns (nx)
+    int widthResolution = 800;
 
-    int wResolution = 1024;
-    int hResolution = 768;
-    // #rows = hResolution, #cols = wResolution
-    Image<Colour> image(hResolution, wResolution);
+    // number of rows (ny)
+    int heightResolution = 600;
 
-    /// TODO: define camera position and sphere position here
-    // example: Vec3 spherePos = Vec3(0, 5, 0);
+    Image<Colour> image(heightResolution, widthResolution);
 
-    for (int row = 0; row < image.rows(); ++row) {
-        for (int col = 0; col < image.cols(); ++col) {
+    // e
+    Vec3 cameraPosition = Vec3(0, 0, 0);
 
+    Vec3 spherePosition = Vec3(0, 5, 0);
+
+    for (int row = 0; row < image.rows(); row++) {
+        for (int col = 0; col < image.cols(); col++) {
             /// TODO: build primary rays
             // example: Vec3 o = Vec3(0,0,0);
+
+            // Vec3 ray = Vec3(0,0,0);
+
+            // Compute u and v
+            // u = l + (r − l) * (i + 0.5) / nx
+            // v = b + (t − b) * (j + 0.5) / ny
+
+            // Compute ray direction
+            // d is image plane distance (i.e. focal length)
+            // ray direction = −d*w + u*u + v*v
+
+            // ray origin is camera position (e)
             
             /// TODO: ray sphere intersection and shading
 
-            /// EXAMPLE: using "image(row,col) = colour;" to set pixel values
-            if (row > 0 && row < 200 && col > 0 && col < 200)  {
-                image(row, col) = red();
-            } else {
-                image(row, col) = white();
-            }
-       }
+            /// EXAMPLE: use "image(row, col) = colour;" to set pixel values
+            // if (row > 0 && row < 200 && col > 0 && col < 200)  {
+            //     image(row, col) = red();
+            // } else {
+            //     image(row, col) = white();
+            // }
+            image(row, col) = white();
+        }
     }
 
     bmpwrite("./out.bmp", image);
