18734a6647c4794a0722e5545cd4cc6f740424b4
Michael Reiter <michaelreiter@live.ca>
2018-02-19 19:21:13 -0800

    Readd to rename

---
+++ assignment2/Shaders/fragment_shader.glsl
@@ -0,0 +1,26 @@
+#version 330 core
+
+uniform vec3 camera_position;
+uniform vec3 light_direction;
+uniform vec3 light_color;
+uniform vec3 ambient_coefficient;
+uniform vec3 object_ambient;
+uniform vec3 object_diffuse;
+uniform vec3 object_specular;
+uniform float phong_exponent;
+
+in vec3 fposition;
+in vec3 fnormal;
+
+out vec4 fcolor;
+
+void main() {
+    vec3 normal = normalize(fnormal);
+    vec3 view = normalize(camera_position - fposition);
+    float diffuse_coefficient = max(-dot(light_direction, normal), 0);
+    float specular_coefficient = pow(max(dot(reflect(view, normal), light_direction), 0), phong_exponent);
+    vec3 phong_lighting = ambient_coefficient * object_ambient
+                        + diffuse_coefficient * light_color * object_diffuse
+                        + specular_coefficient * light_color * object_specular;
+    fcolor = vec4(phong_lighting, 1);
+}
