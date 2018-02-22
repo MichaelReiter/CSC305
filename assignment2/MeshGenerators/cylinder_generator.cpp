3e1cab147e073e6abb0638908cb9dd8632f14b3f
Michael Reiter <michaelreiter@live.ca>
2018-02-22 11:42:47 -0800

    Add cylinder normals

--- assignment2/MeshGenerators/cylinder_generator.cpp
+++ assignment2/MeshGenerators/cylinder_generator.cpp
@@ -10,32 +10,41 @@ namespace MeshGenerators {
     void CylinderGenerator::write_to_file(std::ofstream& file) const
     {
         file << "# cylinder" << std::endl;
+        OpenGP::Vec3 north_pole = {0.0f, 1.0f, 0.0f};
+        OpenGP::Vec3 south_pole = {0.0f, -1.0f, 0.0f};
         std::vector<OpenGP::Vec3> vertices;
+        std::vector<OpenGP::Vec3> normals;
         // Points around circles
         for (int i = 0; i < m_resolution; i++) {
             float theta = (2 * M_PI * i) / (m_resolution - 1);
-            vertices.push_back({sin(theta), 1.0f, cos(theta)});
-            vertices.push_back({sin(theta), -1.0f, cos(theta)});
+            OpenGP::Vec3 v1 = {sin(theta), 1.0f, cos(theta)};
+            OpenGP::Vec3 v2 = {sin(theta), -1.0f, cos(theta)};
+            vertices.push_back(v1);
+            vertices.push_back(v2);
+            normals.push_back(v1 - north_pole);
+            normals.push_back(v2 - south_pole);
         }
         // North and South poles in the center of the circles
         vertices.push_back({0.0f, 1.0f, 0.0f});
         vertices.push_back({0.0f, -1.0f, 0.0f});
+        normals.push_back({0.0f, 1.0f, 0.0f});
+        normals.push_back({0.0f, -1.0f, 0.0f});
+        write_vertex_normals(file, normals);
         write_vertices(file, vertices);
-        std::vector<OpenGP::Vec3> faces;
+        std::vector<OpenGP::Vec3> vertex_indices;
         // Faces on the rectangle
         for (int i = 1; i < (2 * m_resolution); i++) {
-            faces.push_back({(i), (i + 1), (i + 2)});
-            faces.push_back({(i + 1), (i + 3), (i + 2)});
+            vertex_indices.push_back({(i), (i + 1), (i + 2)});
+            vertex_indices.push_back({(i + 1), (i + 3), (i + 2)});
         }
         // Faces on the circles
         for (int i = 1; i < (2 * m_resolution - 1); i++) {
             if (i % 2 == 0) {
-                faces.push_back({i, (2 * m_resolution + 2), (i + 2)});
+                vertex_indices.push_back({i, (2 * m_resolution + 2), (i + 2)});
             } else {
-                faces.push_back({(i + 2), (2 * m_resolution + 1), i});
+                vertex_indices.push_back({(i + 2), (2 * m_resolution + 1), i});
             }
         }
-        std::vector<OpenGP::Vec3> normals;
-        // write_faces(file, faces, normals);
+        write_faces(file, vertex_indices, vertex_indices);
     }
 }
