167d20b211ff0488d1936b960df7cf1463c68760
Michael Reiter <michaelreiter@live.ca>
2018-02-22 14:05:44 -0800

    Increase sphere resolution

--- assignment2/Shaders/fragment_shader.glsl
+++ assignment2/Shaders/fragment_shader.glsl
@@ -17,8 +17,8 @@ out vec4 fragment_color;
 void main() {
     vec3 normal = normalize(fragment_normal);
     vec3 view = normalize(camera_position - fragment_position);
-    float diffuse_coefficient = max(-dot(light_direction, normal), 0);
-    float specular_coefficient = pow(max(dot(reflect(view, normal), light_direction), 0), phong_exponent);
+    float diffuse_coefficient = max(0, -dot(light_direction, normal));
+    float specular_coefficient = pow(max(0, dot(reflect(view, normal), light_direction)), phong_exponent);
     vec3 phong_lighting = ambient_coefficient * object_ambient
                         + diffuse_coefficient * light_color * object_diffuse
                         + specular_coefficient * light_color * object_specular;
